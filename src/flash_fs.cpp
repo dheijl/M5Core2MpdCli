7f56426966cd2d3a3eb609697600fda9977ea6ec
dheijl <danny.heijl@telenet.be>
2024-06-15 17:32:38 +0200

    bugfix saving hostname/ip to nvs

--- src/flash_fs.cpp
+++ src/flash_fs.cpp
@@ -93,7 +93,7 @@ bool NVS_Config::write_players(const PLAYERS& players)
     int i = 0;
     for (auto pl : players) {
         string key = std::to_string(i);
-        String data = String(pl->player_name) + "|" + String(pl->player_ip) + "|" + String(std::to_string(pl->player_port).c_str());
+        String data = String(pl->player_name) + "|" + String(pl->player_hostname) + "|" + String(std::to_string(pl->player_port).c_str());
         if (prefs.putString(key.c_str(), data) == 0) {
             result = false;
             tft_println_error("players prefs put error");
@@ -129,7 +129,8 @@ bool NVS_Config::read_players(PLAYERS& players)
             if (parts.size() == 3) {
                 auto mpd = new MPD_PLAYER();
                 mpd->player_name = strdup(parts[0].c_str());
-                mpd->player_ip = strdup(parts[1].c_str());
+                mpd->player_hostname = strdup(parts[1].c_str());
+                mpd->player_ip = NULL;
                 mpd->player_port = stoi(parts[2]);
                 players.push_back(mpd);
                 tft_println(String(mpd->player_name) + " " + String(mpd->player_ip) + ":" + String(mpd->player_port));
